e0ca4101e5b79a953096457e1f6cbe017a882d91
Kris86r <pechenka869@gmail.com>
2023-03-08 16:48:19 +0500

    Load

--- src/main/java/com/kris/javaYlab/taskGuess/Guess.java
+++ src/main/java/com/kris/javaYlab/taskGuess/Guess.java
@@ -5,28 +5,28 @@ import java.util.Scanner;
 
 public class Guess {
     public static void main(String[] args) throws Exception {
-        int number = 6;
-      //  int number = new Random().nextInt(99) + 1;
+        int number = new Random().nextInt(99) + 1;
         int maxAttempts = 10;
         System.out.println("Я загадал число. У тебя " + maxAttempts + " попыток угадать.");
         Scanner scanner = new Scanner(System.in);
         int N = 0;
-        while (maxAttempts != 0) {
+        do {
+            int num = scanner.nextInt();
+            N++;
+            maxAttempts--;
             if (maxAttempts == 0) {
                 System.out.println("Ты не угадал");
                 break;
             }
-                int num = scanner.nextInt();
-                N++;
-                maxAttempts--;
-                if (num == number) {
-                    System.out.println("Ты угадал с " + N + " попытки");
-                   break;
-                } else if (num > number) {
-                    System.out.println("Мое число меньше! У тебя осталось " + maxAttempts + " попыток");
-                } else {
-                    System.out.println("Мое число больше! У тебя осталось " + maxAttempts + " попыток");
+            if (num == number) {
+                System.out.println("Ты угадал с " + N + " попытки");
+                break;
+            } else if (num > number) {
+                System.out.println("Мое число меньше! У тебя осталось " + maxAttempts + " попыток");
+            } else {
+                System.out.println("Мое число больше! У тебя осталось " + maxAttempts + " попыток");
             }
         }
+        while (maxAttempts != 0);
     }
 }
