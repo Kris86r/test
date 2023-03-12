9f9716818db607eba0dbd246d71803e3ad6278ce
Kris86r <pechenka869@gmail.com>
2023-03-12 15:06:02 +0500

    Load

---
+++ src/main/java/com/javarush/task/pro/task10/task1012/Memory.java
@@ -0,0 +1,55 @@
+package com.javarush.task.pro.task10.task1012;
+
+import java.util.Arrays;
+
+public class Memory {
+
+    public static void main(String[] args) {
+        String[] memory = {"object15", null, null, "object2", null, null, null, "object32", null, "object4"};
+        executeDefragmentation(memory);
+        System.out.println(Arrays.toString(memory));
+    }
+
+    public static void executeDefragmentation(String[] array) {
+        String[] ar = new String[array.length];
+        for (int i = 0, j = 0; i < array.length; i++) {
+            if (array[i] != null) {
+                ar[j++] = array[i];
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            array[i] = ar[i];
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
