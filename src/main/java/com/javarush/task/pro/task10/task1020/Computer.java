093ed4de5bc4f2253ad0edc3e680436ab8a3d5a2
Kris86r <pechenka869@gmail.com>
2023-03-13 13:55:41 +0500

    Load

---
+++ src/main/java/com/javarush/task/pro/task10/task1020/Computer.java
@@ -0,0 +1,39 @@
+package com.javarush.task.pro.task10.task1020;
+
+public class Computer {
+    SystemUnit systemUnit = new SystemUnit();
+    Keyboard keyboard = new Keyboard();
+    Mouse mouse = new Mouse();
+    Monitor monitor = new Monitor();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/javarush/task/pro/task10/task1020/Keyboard.java
@@ -0,0 +1,34 @@
+package com.javarush.task.pro.task10.task1020;
+                  
+public class Keyboard {
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
