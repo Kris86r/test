e5b5bc735d93bd9cac162df57c35890b0738400a
Kris86r <pechenka869@gmail.com>
2023-03-17 20:56:06 +0500

    Load

---
+++ src/main/java/com/javarush/task/pro/task11/task1104/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.pro.task11.task1104;
+
+public class Solution {
+    public static void main(String[] args) {
+        String string = "12.84";
+        double str = Math.round(Double.parseDouble(string));
+        System.out.println((int) str);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
