d57a604addf4c78ab875008f5c12185e5de62b9e
Kris86r <pechenka869@gmail.com>
2023-03-17 21:22:32 +0500

    Load

---
+++ src/main/java/com/javarush/task/pro/task11/task1131/BagoRap.java
@@ -0,0 +1,36 @@
+package com.javarush.task.pro.task11.task1131;
+         
+public class BagoRap {
+    public static int score;
+    public int volume = 2;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
