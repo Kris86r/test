f5f1819a217af2663cfe1907e225f9e3e7f45823
Kris86r <pechenka869@gmail.com>
2023-03-20 14:19:18 +0500

    Load

---
+++ src/main/java/com/javarush/task/pro/task11/task1126/Student.java
@@ -0,0 +1,39 @@
+package com.javarush.task.pro.task11.task1126;
+
+class Student {
+    public String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
