0aa53340d710e1426268bb3a07108b8241cac517
Kris86r <pechenka869@gmail.com>
2023-03-25 20:03:13 +0500

    Load

---
+++ src/main/java/com/kris/JavaBook/TestArrays.java
@@ -0,0 +1,27 @@
+package com.kris.JavaBook;
+
+public class TestArrays {
+    public static void main(String[] args) {
+        String[] islands = new String[4];
+        islands[0] = "Бермунды";
+        islands[1] = "фиджи";
+        islands[2] = "Азовские острова";
+        islands[3] = "Косумель";
+
+        int[] index = new int[4];
+        index[0] = 1;
+        index[1] = 3;
+        index[2] = 0;
+        index[3] = 2;
+
+        int y = 0;
+        int ref;
+        while (y < 4) {
+            ref = index[y];
+            System.out.print("острова = ");
+            System.out.println(islands[ref]);
+            y = y + 1;
+        }
+
+    }
+}
