81206d8341735a38fe7e173b17f1cbf3c8b84a8d
Kris86r <pechenka869@gmail.com>
2023-03-31 12:35:54 +0500

    Load

---
+++ src/main/java/com/javarush/task/pro/task12/task1219/Solution.java
@@ -0,0 +1,47 @@
+package com.javarush.task.pro.task12.task1219;
+
+import java.util.ArrayList;
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        var strings = new ArrayList<String>() {{
+            add("Так");
+            add("тоже");
+            add("можно");
+            add("делать");
+            add("!");
+        }};
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
