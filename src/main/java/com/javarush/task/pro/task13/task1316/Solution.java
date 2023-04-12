218f162b137da7b05fc3db137db773a923648886
Kris86r <pechenka869@gmail.com>
2023-04-12 12:40:42 +0500

    Load

---
+++ src/main/java/com/javarush/task/pro/task13/task1316/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.pro.task13.task1316;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        for (JavarushQuest javarushQuest : JavarushQuest.values()) {
+            System.out.println(javarushQuest.ordinal());
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
