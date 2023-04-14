654ffecf3a3227cd2b09739a1b87b8880b1b95c6
Kris86r <pechenka869@gmail.com>
2023-04-14 12:24:27 +0500

    Load

---
+++ src/main/java/com/javarush/task/pro/task13/task1313/StringLinkedList.java
@@ -0,0 +1,66 @@
+package com.javarush.task.pro.task13.task1313;
+
+public class StringLinkedList {
+    private Node first = new Node();
+    private Node last = new Node();
+
+    public void printAll() {
+        Node currentElement = first.next;
+        while ((currentElement) != null) {
+            System.out.println(currentElement.value);
+            currentElement = currentElement.next;
+        }
+    }
+
+    public StringLinkedList() {
+        first.next = last;
+        last.prev = first;
+    }
+
+    public void add(String value) {
+        Node node = new Node();
+        node.next = last;
+        node.prev = last.prev;
+        node.prev.next = node;
+        last.prev = node;
+        node.value = value;
+    }
+
+    public static class Node {
+        private Node prev;
+        private String value;
+        private Node next;
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
