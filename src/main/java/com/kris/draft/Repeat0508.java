48f55d91fdd4ef600c34eec553aa5c2bad987bee
Kris86r <pechenka869@gmail.com>
2023-04-17 19:42:55 +0500

    Load

--- src/main/java/com/kris/draft/Repeat0508.java
+++ src/main/java/com/kris/draft/Repeat0508.java
@@ -9,14 +9,15 @@ public class Repeat0508 {
         strings = new String[6];
         Scanner scanner = new Scanner(System.in);
         int count = 0;
-        while (count < 6) {
-            strings[count] = scanner.nextLine();
-            count++;
+        while (count < strings.length) {
+            strings[count++] = scanner.nextLine();
         }
+        scanner.close();
         for (int i = 0; i < strings.length; i++) {
-            String copy = strings[i];
+            if (strings[i] == null) continue;
+            String duplicate = strings[i];
             for (int j = i + 1; j < strings.length; j++) {
-                if (copy != null && copy.equals(strings[j])) {
+                if (duplicate.equals(strings[j])) {
                     strings[j] = null;
                     strings[i] = null;
                 }
