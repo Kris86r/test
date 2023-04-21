ae5b93697d621645fa2fce7e930282421ef708d8
Kris86r <pechenka869@gmail.com>
2023-04-21 20:04:53 +0500

    Load

---
+++ src/main/java/com/kris/JavaBook/Boats/Rowboat.java
@@ -0,0 +1,9 @@
+package com.kris.JavaBook.Boats;
+
+public class Rowboat extends Boat {
+    public void rowTheBoat() {
+        System.out.print("Давай, Наташа!");
+    }
+}
+
+
---
+++ src/main/java/com/kris/JavaBook/Boats/Sailboat.java
@@ -0,0 +1,7 @@
+package com.kris.JavaBook.Boats;
+
+public class Sailboat extends Boat {
+    public void move() {
+        System.out.print("Поднять паруса!");
+    }
+}
