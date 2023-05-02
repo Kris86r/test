acb8fd1572302fb79a8086e8590b4cda1a2900af
Kris86r <pechenka869@gmail.com>
2023-05-02 10:55:55 +0500

    Load

--- src/main/java/com/kris/JavaBook/GUI/BeatBox.java
+++ src/main/java/com/kris/JavaBook/GUI/BeatBox.java
@@ -3,13 +3,12 @@ package com.kris.JavaBook.GUI;
 import javax.sound.midi.*;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
+import java.awt.event.*;
+import java.util.*;
 
 public class BeatBox {
     JPanel mainPanel;
-    ArrayList<JCheckBox> checkboxList;
+    ArrayList<JCheckBox> checkboxList;  // Мы храним флажки в массиве ArrayList.
     Sequencer sequencer;
     Sequence sequence;
     Track track;
@@ -18,8 +17,9 @@ public class BeatBox {
     String[] instrumentNames = {"Bass Drum", "Closed Hi-Hat", "Open Hit-Hat", "Acoustic Snare", "Crash Cymbal",
             "Hand Clap", "High Tom", "Hi Bongo", "Maracas", "Whistle", "Low Conga", "Cowbell", "Vibraslap",
             "Low-mid Tom", "High Agogo", "Open Hi Conga"};
-    int[] instruments = {35, 42, 46, 38, 49, 39, 50, 60, 70, 72, 64, 56, 58, 47, 67, 63};
-
+    int[] instruments = {35, 42, 46, 38, 49, 39, 50, 60, 70, 72, 64, 56, 58, 47, 67, 63}; // Эти числа представляют
+    // собой фактические барабанные клавиши. Канал барабана - это что-то вроде фортопиано, только каждая клавиша на
+    // нем - отдельный барабан. Номер 35 - это клавиша для Bass drum, а 42 - Closed Hi-Hat и т.д.
     public static void main(String[] args) {
         new BeatBox().buildGUI();
     }
@@ -30,6 +30,7 @@ public class BeatBox {
         BorderLayout layout = new BorderLayout();
         JPanel background = new JPanel(layout);
         background.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        // (createEmptyBorder) пустая граница позволяет создать поля между краями панели и местом размещения компонентов
 
         checkboxList = new ArrayList<JCheckBox>();
         Box buttonBox = new Box(BoxLayout.Y_AXIS);
@@ -66,9 +67,9 @@ public class BeatBox {
         mainPanel = new JPanel(grid);
         background.add(BorderLayout.CENTER, mainPanel);
 
-        for (int i = 0; i < 256; i++) {
-            JCheckBox c = new JCheckBox();
-            c.setSelected(false);
+        for (int i = 0; i < 256; i++) {     // Создаем флажки, присваиваем им значения false (чтобы они не были
+            JCheckBox c = new JCheckBox();   // установленны), а затем добавляем их в массив
+            c.setSelected(false);     //  ArrayList и на панель.
             checkboxList.add(c);
             mainPanel.add(c);
         }
@@ -83,8 +84,8 @@ public class BeatBox {
     public void setUpMidi() {
         try {
             sequencer = MidiSystem.getSequencer();
-            sequencer.open();
-            sequence = new Sequence(Sequence.PPQ, 4);
+            sequencer.open();                                     // MIDI- код для получения
+            sequence = new Sequence(Sequence.PPQ, 4);    // синтезатора, секвенсона и дорожки.
             track = sequence.createTrack();
             sequencer.setTempoInBPM(120);
 
@@ -92,44 +93,46 @@ public class BeatBox {
             e.printStackTrace();
         }
     }
-
+     // Здесь все происходит! Мы преобразуем состояния флажков в  MIDI- события и добавляем их на дорожку.
     public void buildTrackAndStart() {
-        int[] trackList = null;
-
-        sequence.deleteTrack(track);
+        int[] trackList = null;   // Создаем массив из 16 элементов, чтобы хранить значения для каждого
+                          // инструмента, на все 16 тактов.
+        sequence.deleteTrack(track);     // Избавляемся от старой дорожки и создаем новую.
         track = sequence.createTrack();
 
-        for (int i = 0; i < 16; i++) {
+        for (int i = 0; i < 16; i++) {  // Делаем это для каждого из 16 рядов (то есть для Bass, Congo и т.д.).
             trackList = new int[16];
 
-            int key = instruments[i];
-
-            for (int j = 0; j < 16; j++) {
+            int key = instruments[i]; // Задаем клавишу, которая представляет инструмент (Bass, Hi-Hat и т.д.).
+            // Массив содержит MIDI- числа для каждого инструмента.
+            for (int j = 0; j < 16; j++) {   // Делаем это для каждого такта текущего ряда.
                 JCheckBox jc = (JCheckBox) checkboxList.get(j + (16 * i));
-                if (jc.isSelected()) {
-                    trackList[j] = key;
-                } else {
+                if (jc.isSelected()) {  // Установлен ли флажок на данном такте? Если да, то помещаем значение
+                    trackList[j] = key;  // клавиши в текущую ячейку массива (ячейку, которая представляет такт).
+                } else {  // Если нет, то инструмент не должен играть в этом такте, поэтому присваиваем ему 0.
                     trackList[j] = 0;
                 }
             }
 
-            makeTracks(trackList);
-            track.add(makeEvent(176, 1, 127, 0, 16));
+            makeTracks(trackList);  // Для этого инструмента и для всех 16 тактов создаем события
+            track.add(makeEvent(176, 1, 127, 0, 16)); // и добавляем их на дорожку.
         }
 
-        track.add(makeEvent(192, 9, 1, 0, 15));
-        try {
-
+        track.add(makeEvent(192, 9, 1, 0, 15)); // Мы всегда должны быть уверены, что
+        try {  // событие на такте 16 существует (они идут от 0 до 15). Иначе BeatBox может не пройти все
+               // 16 тактов, перед тем как заново начнет последовательность.
             sequencer.setSequence(sequence);
-            sequencer.setLoopCount(sequencer.LOOP_CONTINUOUSLY);
+            sequencer.setLoopCount(sequencer.LOOP_CONTINUOUSLY);  // Позволяет задать количество повторений
+            // цикла или, как в этом случае, непрерывный цикл.
             sequencer.start();
             sequencer.setTempoInBPM(120);
         } catch (Exception e) {
-            e.printStackTrace();
+            e.printStackTrace();  // Теперь мы проигрываем мелодию.
         }
     }
 
-    public class MyStartListener implements ActionListener {
+    public class MyStartListener implements ActionListener { // Первый из внутренних классов -
+        // слушателей для кнопок.
         @Override
         public void actionPerformed(ActionEvent e) {
             buildTrackAndStart();
@@ -147,9 +150,9 @@ public class BeatBox {
         @Override
         public void actionPerformed(ActionEvent e) {
             float tempoFactor = sequencer.getTempoFactor();
-            sequencer.setTempoFactor((float) (tempoFactor * 1.03));
-        }
-    }
+            sequencer.setTempoFactor((float) (tempoFactor * 1.03)); // Коэффициент темпа определяет темп
+        }  // синтезатора. По умолчанию он равен 1.0, поэтому щелчком кнопкой мыши можно изменить
+    }      // его на +/- 3%.
 
     public class MyDownTempoListener implements ActionListener {
         @Override
@@ -158,15 +161,16 @@ public class BeatBox {
             sequencer.setTempoFactor((float) (tempoFactor * .97));
         }
     }
-
-    public void makeTracks(int[] list) {
-        for (int i = 0; i < 16; i++) {
+// Метод создает события для одного инструмента за каждый проход цикла для всех 16 тактов. Можно получить int[]
+    // для Bass drum, и каждый элемент массива будет содержать либо клавишу этого инструмента, либо ноль. Если
+    public void makeTracks(int[] list) {   // это ноль, то инструмент не доллжен играть на текущем такте. Иначе
+        for (int i = 0; i < 16; i++) { // нужно создать событие и добавить его в дорожку.
             int key = list[i];
 
             if (key != 0) {
-                track.add(makeEvent(144, 9, key, 100, i));
-                track.add(makeEvent(128, 9, key, 100, i + 1));
-            }
+                track.add(makeEvent(144, 9, key, 100, i)); // Создаем события включения и
+                track.add(makeEvent(128, 9, key, 100, i + 1)); // выключения и добавляем
+            }                        // их в дорожку.
         }
     }
 
