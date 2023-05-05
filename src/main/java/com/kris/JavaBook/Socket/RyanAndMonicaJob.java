a4501b226a3f8080c8d141ba08217941eefbf84c
Kris86r <pechenka869@gmail.com>
2023-05-05 19:24:02 +0500

    Load

--- src/main/java/com/kris/JavaBook/Socket/RyanAndMonicaJob.java
+++ src/main/java/com/kris/JavaBook/Socket/RyanAndMonicaJob.java
@@ -22,7 +22,7 @@ public class RyanAndMonicaJob implements Runnable {
         }
     }
 
-    private void makeWithdrawal(int amount) {
+    private synchronized void makeWithdrawal(int amount) {
         if (account.getBalance() >= amount) {
             System.out.println(Thread.currentThread().getName() + " собирается снять деньги");
             try {
