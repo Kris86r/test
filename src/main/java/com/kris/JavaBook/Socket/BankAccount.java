3557e358ed81b0d0835bc2a5c761d0303d868749
Kris86r <pechenka869@gmail.com>
2023-05-05 19:13:07 +0500

    Load

---
+++ src/main/java/com/kris/JavaBook/Socket/BankAccount.java
@@ -0,0 +1,13 @@
+package com.kris.JavaBook.Socket;
+
+class BankAccount {
+    private int balance = 100;
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void withdraw(int amount) {
+        balance = balance - amount;
+    }
+}
