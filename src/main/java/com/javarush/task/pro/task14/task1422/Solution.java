57bad82f4123689b48ec6842149817287b9efe25
Kris86r <pechenka869@gmail.com>
2023-05-14 15:40:42 +0500

    Load

---
+++ src/main/java/com/javarush/task/pro/task14/task1422/Solution.java
@@ -0,0 +1,74 @@
+package com.javarush.task.pro.task14.task1422;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        printDNA();
+    }
+
+    public static void printDNA() {
+        for (int s = 0; s < getDNA().size(); s++) {
+            System.out.println("File name: " + getDNA().get(s)[1].getFileName() + ", Method name: " + getDNA().get(s)[1].getMethodName() + ", Line number: " + getDNA().get(s)[1].getLineNumber());
+        }
+    }
+
+    private static List<StackTraceElement[]> getDNA() {
+        List<StackTraceElement[]> list = new ArrayList<>();
+
+        list.add(adenine());
+        list.add(thymine());
+        list.add(cytosine());
+        list.add(guanine());
+
+        return list;
+    }
+
+    private static StackTraceElement[] adenine() {
+        return Thread.currentThread().getStackTrace();
+    }
+
+    private static StackTraceElement[] thymine() {
+        return Thread.currentThread().getStackTrace();
+    }
+
+    private static StackTraceElement[] cytosine() {
+        return Thread.currentThread().getStackTrace();
+    }
+
+    private static StackTraceElement[] guanine() {
+        return Thread.currentThread().getStackTrace();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
