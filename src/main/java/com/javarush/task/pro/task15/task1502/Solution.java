665376069fd865a074c24deff13969b40d514f1b
Kris86r <pechenka869@gmail.com>
2023-05-16 17:23:55 +0500

    Load

---
+++ src/main/java/com/javarush/task/pro/task15/task1502/Solution.java
@@ -0,0 +1,52 @@
+package com.javarush.task.pro.task15.task1502;
+
+import java.util.Scanner;
+
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(System.in);
+            String line = scanner.nextLine();
+            System.out.println(line.toUpperCase());
+        } catch (Exception e) {
+            System.out.println("Something went wrong : " + e);
+        } finally {
+            if (scanner != null) {
+                scanner.close();
+            }
+        }
+    }
+}
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
