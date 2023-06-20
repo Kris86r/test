9a385792bdc2cb319ce8d653ef9ea4e9ae315db1
Kris86r <pechenka869@gmail.com>
2023-06-20 14:29:13 +0500

    Load

---
+++ src/main/java/com/javarush/task/pro/task17/task1702/Shape.java
@@ -0,0 +1,37 @@
+package com.javarush.task.pro.task17.task1702;
+
+public class Shape {
+    public void printInfo(){
+        System.out.println("Геометрическая фигура");
+    }
+}
+
+
+
+                                             
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
