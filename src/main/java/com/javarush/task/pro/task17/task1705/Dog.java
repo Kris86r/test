000b398650efd947eb1b204c24205b8b8afa5c57
Kris86r <pechenka869@gmail.com>
2023-06-20 17:52:11 +0500

    Load

---
+++ src/main/java/com/javarush/task/pro/task17/task1705/Dog.java
@@ -0,0 +1,37 @@
+package com.javarush.task.pro.task17.task1705;
+
+public class Dog extends Astronaut {
+    public String getInfo() {
+        return "Собака";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
