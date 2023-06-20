446bc0195a1c0a6b9ba2631927bd6daf0966ada7
Kris86r <pechenka869@gmail.com>
2023-06-20 14:22:54 +0500

    Load

---
+++ src/main/java/com/javarush/task/pro/task17/task1701/Animal.java
@@ -0,0 +1,37 @@
+package com.javarush.task.pro.task17.task1701;
+
+public class Animal {
+    public void move(){
+        System.out.println("Я бегу!");
+    }
+}
+
+
+                                      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
