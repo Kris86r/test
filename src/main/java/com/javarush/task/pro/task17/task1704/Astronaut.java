acbc8d918efcfa396df65f8033469fffd3999ec4
Kris86r <pechenka869@gmail.com>
2023-06-20 17:45:47 +0500

    Load

---
+++ src/main/java/com/javarush/task/pro/task17/task1704/Astronaut.java
@@ -0,0 +1,37 @@
+package com.javarush.task.pro.task17.task1704;
+
+public class Astronaut {
+    public String getInfo(){
+        return "Неизвестный астронавт";
+    }              
+}
+
+
+
+
+
+
+
+
+
+                                     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/javarush/task/pro/task17/task1704/Dog.java
@@ -0,0 +1,37 @@
+package com.javarush.task.pro.task17.task1704;
+
+public class Dog extends Astronaut {
+    public String getInfo() {
+        return "Собака";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
