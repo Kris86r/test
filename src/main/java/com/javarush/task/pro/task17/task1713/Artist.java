f43d54a716b50fd25c7e585cfd77088ffdc96d68
Kris86r <pechenka869@gmail.com>
2023-06-21 14:25:41 +0500

    Load

---
+++ src/main/java/com/javarush/task/pro/task17/task1713/Artist.java
@@ -0,0 +1,38 @@
+package com.javarush.task.pro.task17.task1713;
+
+/* 
+Да начнется представление! 
+*/
+
+public class Artist implements Dance, Sing {
+}
+
+
+
+
+
+
+
+
+
+                                       
+
+
+
+                                      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/javarush/task/pro/task17/task1713/Dance.java
@@ -0,0 +1,34 @@
+package com.javarush.task.pro.task17.task1713;
+
+public interface Dance {
+}
+
+
+                                   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
