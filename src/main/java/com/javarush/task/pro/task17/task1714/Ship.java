6b642df49844d983bf6eaac18077092fe25ce5e9
Kris86r <pechenka869@gmail.com>
2023-06-21 14:54:31 +0500

    Load

---
+++ src/main/java/com/javarush/task/pro/task17/task1714/Ship.java
@@ -0,0 +1,34 @@
+package com.javarush.task.pro.task17.task1714;
+
+public class Ship  implements  Swimmable{
+}
+
+
+
+
+                                      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/javarush/task/pro/task17/task1714/Zeppelin.java
@@ -0,0 +1,34 @@
+package com.javarush.task.pro.task17.task1714;
+
+public class Zeppelin implements Flyable {
+}
+
+                                                                      
+
+
+                      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
