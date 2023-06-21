3ed7d7bc861a9b4589c62845e446699423b3d55b
Kris86r <pechenka869@gmail.com>
2023-06-21 15:13:14 +0500

    Load

---
+++ src/main/java/com/javarush/task/pro/task17/task1717/Aircraft.java
@@ -0,0 +1,38 @@
+package com.javarush.task.pro.task17.task1717;
+
+public class Aircraft implements Flyable {
+    @Override
+    public int getMaxSpeed() {
+        return 1200;
+    }
+}
+
+
+
+                                                           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/javarush/task/pro/task17/task1717/Flyable.java
@@ -0,0 +1,39 @@
+package com.javarush.task.pro.task17.task1717;
+
+/* 
+Кто летит?
+*/
+                                                  
+public interface Flyable {
+    int getMaxSpeed();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
