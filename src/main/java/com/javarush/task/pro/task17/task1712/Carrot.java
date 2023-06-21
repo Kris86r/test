ae9af9b45d19b9c26fafd7d6054fb7911aaf1a72
Kris86r <pechenka869@gmail.com>
2023-06-21 14:19:29 +0500

    Load

---
+++ src/main/java/com/javarush/task/pro/task17/task1712/Carrot.java
@@ -0,0 +1,34 @@
+package com.javarush.task.pro.task17.task1712;
+
+public class Carrot implements Eatable {
+}
+
+
+
+
+
+                                              
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
