bd86af272d5cf71b19181873a41e977bd242f07d
Kris86r <pechenka869@gmail.com>
2023-06-23 17:50:50 +0500

    Load

---
+++ src/main/java/com/javarush/task/pro/task18/task1823/Solution.java
@@ -0,0 +1,52 @@
+package com.javarush.task.pro.task18.task1823;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/* 
+Из потока данных во множество
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        var stringStream = Stream.of("JavaRush", "CodeGym", "Amigo", "Elly", "Kim", "Risha");
+
+        getFilteredStrings(stringStream).forEach(System.out::println);
+    }
+
+    public static Set<String> getFilteredStrings(Stream<String> stringStream) {
+        return stringStream.filter(x -> x.length() > 6).collect(Collectors.toSet());
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
