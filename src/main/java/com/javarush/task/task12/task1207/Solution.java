f929dabd2323250e17f237f6a53d594b2b15c36e
Kris86r <pechenka869@gmail.com>
2023-07-26 14:56:04 +0500

    Load

---
+++ src/main/java/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,50 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(13);
+        print((Integer) 13);
+    }
+
+    public static void print(int num) {
+
+    }
+
+    public static void print(Integer num) {
+
+    }
+}                                                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,61 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(byte num) {
+
+    }
+
+    public static void print(int num) {
+
+    }
+
+    public static void print(long num) {
+
+    }
+
+    public static void print(String text) {
+
+    }
+
+    public static void print(int num, String text) {
+
+    }
+}                                 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
