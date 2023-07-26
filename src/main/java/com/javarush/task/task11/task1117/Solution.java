4f30b5c9498b19c183f286bc964104a075a667d5
Kris86r <pechenka869@gmail.com>
2023-07-26 13:26:13 +0500

    Load

---
+++ src/main/java/com/javarush/task/task11/task1117/Solution.java
@@ -0,0 +1,60 @@
+package com.javarush.task.task11.task1117;
+
+/* 
+Альтернативная цепочка наследования
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Carnivore {
+
+    }
+
+    public class Cow extends Animal {
+
+    }
+
+    public class Dog extends Carnivore {
+
+    }
+
+    public class Pig extends Animal {
+
+    }
+
+    public class Animal {
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
